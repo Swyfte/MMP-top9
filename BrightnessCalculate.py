79a7d902e399414c1ef89599529d9f9d6889ca5b
top9 <top9@aber.ac.uk>
2019-04-09 13:27:12 +0100

    Many changes

--- BrightnessCalculate.py
+++ BrightnessCalculate.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-import Sub-modules
+import Submodules as sb
+
 
 ## Import the jpg files
 mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
@@ -15,20 +16,6 @@ onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" 
 for f in onlyfiles:
 	filename = f
 	img = cv2.imread(filename)
-
-	ret,thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY) #threshold that lad
-	#The following chunk calculates the white balance
-	height, width, colour = img.shape
-	Size = thresh.size
-	print()
-	print(filename)
-	#print("size " + str(Size))
-	whiteCount = cv2.countNonZero(thresh)
-	#print("w/c " + str(whiteCount))
-	blackCount = Size - whiteCount
-	#print("b/c " + str(blackCount))
-	if float(whiteCount)/Size > 0.5:
-		print("Light, " + str(round((float(whiteCount)/Size)*100, 2)) + "% white")
-	else:
-		print("Dark, " + str(round((float(whiteCount)/Size)*100, 2)) + "% white")
-	print("height and width :", height, width)
+	grey = sb.grey(img)
+	ret,thresh = sb.thresh(grey)
+	sb.whiteCount(thresh,filename)
