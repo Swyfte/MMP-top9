4ba4c567b916dbc7d3e5a9143a48add167e03e73
top9 <top9@aber.ac.uk>
2019-04-10 22:01:14 +0100

    Rearranged images for testing

--- VanishingPoint.py
+++ VanishingPoint.py
@@ -14,14 +14,14 @@ for f in onlyfiles:
 	filename = f
 	img = cv2.imread(filename)
 	height, width, colour = img.shape
-	percent = 75
+	percent = 25
 	dim = (int(width * percent/100), int(height * percent/100))
+	kernel = np.ones((15,15), np.uint8)
 
 	blur = cv2.GaussianBlur(img,(5,5),0)
-	new_img = np.zeros(img.shape, img.dtype)
-	new_img = cv2.convertScaleAbs(blur,alpha=2.0,beta=0)
-	edges = cv2.Canny(new_img,200,250)
-	lines = cv2.HoughLines(edges, 2, np.pi/180, 200, 100, 200)
+	opening = cv2.morphologyEx(blur, cv2.MORPH_OPEN, kernel)
+	edges = cv2.Canny(opening,50,150,apertureSize = 3)
+	lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
 	imgSML = cv2.resize(img, dim)
 	edgesSML = cv2.resize(edges, dim)
 	cv2.imshow("edges",edgesSML)
@@ -33,13 +33,12 @@ for f in onlyfiles:
 			b = np.sin(theta)
 			x0 = a*rho
 			y0 = b*rho
-			x1 = int(x0 + 1000*(-b))
-			y1 = int(y0 + 1000*(a))
-			x2 = int(x0 - 1000*(-b))
-			y2 = int(y0 - 1000*(a))
+			x1 = int(x0 + 5000*(-b))
+			y1 = int(y0 + 5000*(a))
+			x2 = int(x0 - 5000*(-b))
+			y2 = int(y0 - 5000*(a))
 			if (x1 != x2):
-				cv2.line(edges,(x1,y1),(x2,y2),(255,255,0),2)
-	edgeshowSML = cv2.resize(edges, dim)
+				cv2.line(img,(x1,y1),(x2,y2),(255,255,0),2)
+	edgeshowSML = cv2.resize(img, dim)
 	cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", edgeshowSML)
-	
 	cv2.waitKey(0)
