4aa8baa88345bbab67bcb1a7ea7e7eef320fb877
top9 <top9@aber.ac.uk>
2019-04-12 19:43:29 +0100

    Added Ethics document, report and updated VP

--- VanishingPoint.py
+++ VanishingPoint.py
@@ -5,6 +5,23 @@ import random
 import math
 import Submodules as sb
 
+def det(a, b):
+    return a[0] * b[1] - a[1] * b[0]
+
+def line_intersection(line1, line2):
+    x_diff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    y_diff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    div = det(x_diff, y_diff)
+    if div == 0:
+        return None  # Lines don't cross
+
+    d = (det(*line1), det(*line2))
+    x = det(d, x_diff) / div
+    y = det(d, y_diff) / div
+
+    return x, y
+
 mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
 filename = ""
 from os import listdir
@@ -24,7 +41,7 @@ for f in onlyfiles:
 	opening = cv2.morphologyEx(blur, cv2.MORPH_OPEN, kernel)
 	edges = cv2.Canny(opening,50,150,apertureSize = 3)
 	lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
-	
+
 	if lines:
 		if len(lines) > size:
 			size = len(lines)
