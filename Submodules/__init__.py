d347a4a14509695c19560d94ff1cb20159fa928f
top9 <top9@aber.ac.uk>
2019-04-15 18:20:16 +0100

    Update __init__.py

--- Submodules/__init__.py
+++ Submodules/__init__.py
@@ -2,6 +2,7 @@ from Submodules.brightContrast import both
 from Submodules.brightContrast import bright
 from Submodules.brightContrast import contrast
 from Submodules.greyscale import grey
+from Submodules.greyscale import greyTest
 from Submodules.greyscale import greySave
 from Submodules.splitColours import colours
 from Submodules.splitColours import remerge
