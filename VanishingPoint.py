fb1317d5ceed21522ac3813172fcc66747e3bf78
top9 <top9@aber.ac.uk>
2019-04-15 15:12:24 +0100

    Updated vanishing points

--- VanishingPoint.py
+++ VanishingPoint.py
@@ -4,29 +4,14 @@ from matplotlib import pyplot as plt
 import random
 import math
 import Submodules as sb
-
-def det(a, b):
-    return a[0] * b[1] - a[1] * b[0]
-
-def line_intersection(line1, line2):
-    x_diff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    y_diff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-    div = det(x_diff, y_diff)
-    if div == 0:
-        return None  # Lines don't cross
-
-    d = (det(*line1), det(*line2))
-    x = det(d, x_diff) / div
-    y = det(d, y_diff) / div
-
-    return x, y
+import vanishing_point as vp
+from itertools import starmap
 
 mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
 filename = ""
 from os import listdir
 from os.path import isfile, join
-onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f))]
+onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and ("centre_" not in f) and (".jpg" in f))]
 
 for f in onlyfiles:
 	filename = f
@@ -35,36 +20,22 @@ for f in onlyfiles:
 	percent = 25
 	dim = (int(width * percent/100), int(height * percent/100))
 	kernel = np.ones((15,15), np.uint8)
-	size = 100
 
 	blur = cv2.GaussianBlur(img,(5,5),0)
-	opening = cv2.morphologyEx(blur, cv2.MORPH_OPEN, kernel)
+	opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 	edges = cv2.Canny(opening,50,150,apertureSize = 3)
-	lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+	lines = cv2.HoughLines(edges, 1, np.pi/180, 300)
+	hough_lines = []
+	if lines is not None:
+		for line in lines:
+			hough_lines.extend(list(starmap(vp.endpoints, line)))
 
-	if lines:
-		if len(lines) > size:
-			size = len(lines)
-		sampleLines = random.sample(lines, size)
-
-	
-
-	imgSML = cv2.resize(img, dim)
-	edgesSML = cv2.resize(edges, dim)
-	cv2.imshow("edges",edgesSML)
-	cv2.imshow("img", imgSML)
-
-	for line in lines:
-		for rho,theta in line:
-			a = np.cos(theta)
-			b = np.sin(theta)
-			x0 = a*rho
-			y0 = b*rho
-			x1 = int(x0 + 5000*(-b))
-			y1 = int(y0 + 5000*(a))
-			x2 = int(x0 - 5000*(-b))
-			y2 = int(y0 - 5000*(a))
-			cv2.line(img,(x1,y1),(x2,y2),(255,255,0),2)
-	edgeshowSML = cv2.resize(img, dim)
-	cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", edgeshowSML)
-	cv2.waitKey(0)
+	if len(lines) > 0:
+		intersect = vp.find_intersections(hough_lines)
+		if intersect:
+			new_img = img.copy()
+			grid_size = min(height, width) // 3
+			vanishpoint = vp.find_vanishing_point(new_img, grid_size, intersect)
+			savename = "centre_" + filename
+			saveLoc = join(mypath, savename)
+			cv2.imwrite(saveLoc, new_img)
