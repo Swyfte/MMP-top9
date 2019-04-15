7803faa29a48cb409c77781a78e74c7e9b87c10f
top9 <top9@aber.ac.uk>
2019-04-15 17:58:57 +0100

    Revert "Update symmetry.py"
    This reverts commit 610356df2526c5d74c722e5403ad9cb8aebdd921.

--- symmetry.py
+++ symmetry.py
@@ -58,7 +58,3 @@ import Submodules as sb
 		cv2.imshow(filename + " SSIM: " + str(sim), cpdSml)
 		#cv2.imshow(filename + " SSIM: " + str(sim), compared)
 		cv2.waitKey(0)
-		if sim > 0.75:
-			return True
-		else:
-			return False
