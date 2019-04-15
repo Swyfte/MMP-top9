b46460ab20cface0d0b878903d55eebc9c95621e
top9 <top9@aber.ac.uk>
2019-04-15 20:31:17 +0100

    Finish blur module
    The module is complete for master branch

--- blurry.py
+++ blurry.py
@@ -13,5 +13,27 @@ from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f))]
 
 for f in onlyfiles:
-    filename = f
-    img = cv2.imread(filename)
+	filename = f
+	img = cv2.imread(filename)
+	height, width, colour = img.shape
+	if width > 1000 or height > 1000:
+		if width > height:
+			scaleBy = 1000/width
+		else:
+			scaleBy = 1000/height
+	else:
+		scaleBy = 1
+	dim = (int(width * scaleBy), int(height * scaleBy))
+	grey = sb.grey(img)
+	text = "Not blurry"
+	threshold = 100
+	blurriness = cv2.Laplacian(grey, cv2.CV_64F).var()
+	if blurriness < threshold:
+		text = "Blurry"
+
+	# show the image
+	cv2.putText(img, "{}: {:.2f}".format(text, blurriness), (10, 60),
+		cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+	imgSml = cv2.resize(img, dim)
+	cv2.imshow(filename, imgSml)
+	cv2.waitKey(0)
