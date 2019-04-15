404a4028470bda565ddcc237b0676c3e9684ad4f
top9 <top9@aber.ac.uk>
2019-04-15 18:08:28 +0100

    Update whiteCount.py

--- Submodules/whiteCount.py
+++ Submodules/whiteCount.py
@@ -1,9 +1,13 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-from os.path import join
 
-def whiteCount(img, filename):
+def whiteCount(img):
+	whiteCount = 0
+	Size = img.size
+	whiteCount = cv2.countNonZero(img)
+	value = (float(whiteCount)/Size)*100
+	return value
+
+def whiteCountTesting(img, filename):
 	blackCount = 0
 	whiteCount = 0
 	height, width = img.shape
@@ -15,8 +19,9 @@ def whiteCount(img, filename):
 	print("w/c " + str(whiteCount))
 	blackCount = Size - whiteCount
 	print("b/c " + str(blackCount))
-	if float(whiteCount)/Size > 0.5:
-		print("white, " + str(round((float(whiteCount)/Size)*100, 2)) + "% white")
+	value = (float(whiteCount)/Size)*100
+	if value > 0.5:
+		print("white, " + str(round(value, 2)) + "% white")
 	else:
-		print("black, " + str(round((float(whiteCount)/Size)*100, 2)) + "% white")
+		print("black, " + str(round(value, 2)) + "% white")
 	print("height and width :", height, width)
