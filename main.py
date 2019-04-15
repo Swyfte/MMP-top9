3f98e6c613808d694a2f85d8a7469311032106f2
top9 <top9@aber.ac.uk>
2019-04-15 19:09:08 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import Submodules as sb
+import cv2
+
+mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
+filename = ""
+from os import listdir
+from os.path import isfile, join
+onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f))]
+
+for f in onlyfiles:
+	filename = f
+	img = cv2.imread(filename)
