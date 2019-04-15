de85459026540e64a8ffb76a0f526a64eb14e846
top9 <top9@aber.ac.uk>
2019-04-15 18:48:11 +0100

    Update colourBal.py

--- colourBal.py
+++ colourBal.py
@@ -2,22 +2,47 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
-filename = ""
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f))]
-
 channels = [
 	([000,000,127],[127,140,255]), #Red
 	([000,127,000],[127,255,160]), #Grn
 	([127,000,000],[255,127,127])  #Blu
 ]
 
-## Loop through and find each colour channel, masking it and counting
-for f in onlyfiles:
-	filename = f
-	img = cv2.imread(filename) 
+def majorColours(img):
+	height, width, colour = img.shape
+	Size = height*width
+	redCount, blueCount, greenCount = 0,0,0
+	loopNum = 0
+	for (lower, upper) in channels:
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+		# Mask the image, filtering each of the colour channels.
+		mask = cv2.inRange(img, lower, upper)
+		masked = cv2.bitwise_and(img, img, mask = mask)
+
+		greyMask = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+		if loopNum == 0:
+			redCount = cv2.countNonZero(greyMask)
+			loopNum += 1
+		elif loopNum == 1:
+			greenCount = cv2.countNonZero(greyMask)
+			loopNum += 1
+		elif loopNum == 2:
+			blueCount = cv2.countNonZero(greyMask)
+			loopNum += 1
+		else:
+			loopNum = 0
+		
+	if (redCount > greenCount) and (redCount > blueCount):
+		return "r"
+	elif (greenCount > redCount) and (greenCount > blueCount):
+		return "g"
+	elif (blueCount > redCount) and (blueCount > greenCount):
+		return "b"
+	else:
+		return "x"
+
+def majorColoursTest(img, filename):
 	height, width, colour = img.shape
 	Size = height*width
 	redCount, blueCount, greenCount = 0,0,0
@@ -46,13 +71,18 @@ for f in onlyfiles:
 		cv2.imshow("image", masked)
 		cv2.waitKey(0)
 	
+	print(filename)
 	print(str(Size))
 	print("r" + str(redCount) + " g" + str(greenCount) + " b" + str(blueCount))
 	if (redCount > greenCount) and (redCount > blueCount):
 		print("Red Major")
+		return "r"
 	elif (greenCount > redCount) and (greenCount > blueCount):
 		print("Green Major")
+		return "g"
 	elif (blueCount > redCount) and (blueCount > greenCount):
 		print("Blue Major")
+		return "b"
 	else:
 		print("No Major")
+		return "x"
