128e764bdb229864d278e9da52e5a1292d54ee52
top9 <top9@aber.ac.uk>
2019-04-15 15:12:53 +0100

    Modulised brightness

--- BrightnessCalculate.py
+++ BrightnessCalculate.py
@@ -1,21 +1,13 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 import Submodules as sb
+import cv2
 
+def calcTest(img, filename):
+	grey = sb.grey(img)
+	ret, thresh = sb.thresh(grey)
+	sb.whiteCountTesting(thresh, filename)
 
-## Import the jpg files
-mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
-filename = ""
-
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f) and ("gray_" not in f))]
-
-## Loop through and make each image greyscale, then thresholded, then count the white
-for f in onlyfiles:
-	filename = f
-	img = cv2.imread(filename)
+def calc(img):
 	grey = sb.grey(img)
 	ret,thresh = sb.thresh(grey)
-	sb.whiteCount(thresh,filename)
+	whiteValue = sb.whiteCount(thresh)
+	return whiteValue
--- Submodules/threshold.py
+++ Submodules/threshold.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 from os.path import join
 
 def threshSave(img, mypath, filename):
