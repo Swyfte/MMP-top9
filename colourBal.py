db86e7c3eefb2b6024bdb905936cbb4e5e8c4ac7
top9 <top9@aber.ac.uk>
2019-04-17 13:50:41 +0100

    Update colourBal.py

--- colourBal.py
+++ colourBal.py
@@ -3,86 +3,83 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 channels = [
-	([000,000,127],[127,140,255]), #Red
-	([000,127,000],[127,255,160]), #Grn
-	([127,000,000],[255,127,127])  #Blu
+	([  0, 46,126], [ 13,255,255]), #Red 1
+	([  7,191,191], [ 40,255,255]), #Orange
+	([  7, 46,126], [ 40,190,190]), #Brown
+	([ 20, 46,126], [ 68,255,255]), #Yellow
+	([ 35, 46,126], [ 67,255,255]), #Green
+	([ 68, 46,126], [ 80,255,255]), #Teal
+	([ 81, 46,126], [ 97,255,255]), #Cyan
+	([ 98, 46,126], [125,255,255]), #Blue
+	([126, 46,126], [141,255,255]), #Purple
+	([142, 46,126], [152,255,255]), #Magenta
+	([153, 46,126], [174,255,255]), #Pink
+	([  0,  0,  0], [180, 45,125]), #Grey
+	([175, 46,126], [180,255,255])  #Red 2
 ]
 
-def majorColours(img):
-	height, width, colour = img.shape
-	Size = height*width
-	redCount, blueCount, greenCount = 0,0,0
-	loopNum = 0
+
+def findColours(img):
+	counts = [
+		["RED", 0],
+		["ORN", 0],
+		["BRN", 0],
+		["YEL", 0],
+		["GRN", 0],
+		["TEL", 0],
+		["CYN", 0],
+		["BLU", 0],
+		["PPL", 0],
+		["MGN", 0],
+		["PNK", 0],
+		["GRY", 0],
+	]
+	hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	i=0
 	for (lower, upper) in channels:
 		lower = np.array(lower, dtype = "uint8")
 		upper = np.array(upper, dtype = "uint8")
 		# Mask the image, filtering each of the colour channels.
-		mask = cv2.inRange(img, lower, upper)
-		masked = cv2.bitwise_and(img, img, mask = mask)
-
+		mask = cv2.inRange(hsvImg, lower, upper)
+		masked = cv2.bitwise_and(hsvImg, hsvImg, mask = mask)
 		greyMask = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
-		if loopNum == 0:
-			redCount = cv2.countNonZero(greyMask)
-			loopNum += 1
-		elif loopNum == 1:
-			greenCount = cv2.countNonZero(greyMask)
-			loopNum += 1
-		elif loopNum == 2:
-			blueCount = cv2.countNonZero(greyMask)
-			loopNum += 1
-		else:
-			loopNum = 0
-		
-	if (redCount > greenCount) and (redCount > blueCount):
-		return "r"
-	elif (greenCount > redCount) and (greenCount > blueCount):
-		return "g"
-	elif (blueCount > redCount) and (blueCount > greenCount):
-		return "b"
-	else:
-		return "x"
 
-def majorColoursTest(img, filename):
-	height, width, colour = img.shape
-	Size = height*width
-	redCount, blueCount, greenCount = 0,0,0
-	loopNum = 0
-	for (lower, upper) in channels:
-		lower = np.array(lower, dtype = "uint8")
-		upper = np.array(upper, dtype = "uint8")
-		# Mask the image, filtering each of the colour channels.
-		mask = cv2.inRange(img, lower, upper)
-		masked = cv2.bitwise_and(img, img, mask = mask)
+		if i < len(counts):
+			counts[i][1] = cv2.countNonZero(greyMask)
+			i += 1
+		elif i == len(counts):
+			counts[0][1] += cv2.countNonZero(greyMask)
+	return counts
 
-		greyMask = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
-		if loopNum == 0:
-			redCount = cv2.countNonZero(greyMask)
-			loopNum += 1
-		elif loopNum == 1:
-			greenCount = cv2.countNonZero(greyMask)
-			loopNum += 1
-		elif loopNum == 2:
-			blueCount = cv2.countNonZero(greyMask)
-			loopNum += 1
-		else:
-			loopNum = 0
+def coloursMain(img):
+	counts = findColours(img)
 
-		# Show the images
-		cv2.imshow("image", masked)
-		cv2.waitKey(0)
-	
-	print(filename)
-	print(str(Size))
-	print("r" + str(redCount) + " g" + str(greenCount) + " b" + str(blueCount))
-	if (redCount > greenCount) and (redCount > blueCount):
-		print("Red Major")
-		return "r"
-	elif (greenCount > redCount) and (greenCount > blueCount):
-		print("Green Major")
-		return "g"
-	elif (blueCount > redCount) and (blueCount > greenCount):
-		print("Blue Major")
-		return "b"
-	else:
-		print("No Major")
-		return "x"
+	Max = ["", 0]
+	for i in counts:
+		if i[1] > Max[1]:
+			Max = i
+
+	return Max
+
+def coloursMainTest(img, filename):
+	counts = findColours(img)
+
+	Max = ["", 0]
+	for i in counts:
+		if i[1] > Max[1]:
+			Max = i
+
+	print("\n" + filename)
+	print(counts)
+	print(Max)
+
+	return Max
+
+def coloursAll(img):
+	return findColours(img)
+
+def coloursAllTest(img, filename):
+	counts = findColours(img)
+	print("\n" + filename)
+	print(counts)
+	return counts
