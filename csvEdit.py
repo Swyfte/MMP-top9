34a019ca260c9473dac08732af1a7d8daa85c7c3
top9 <top9@aber.ac.uk>
2019-04-17 22:15:12 +0100

    Update csvEdit.py
    Made as simple as possible, "quoting" is suprisingly unclear on what it does. But it complains if I remove it.

--- csvEdit.py
+++ csvEdit.py
@@ -2,29 +2,29 @@ import csv
 
 def csvWriteRow(filename, data):
 	with open((filename + ".csv"), mode="w") as csv_file:
-		writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 		writer.writerow(data)
 
 def csvReadRow(filename, row):
 	with open((filename + ".csv")) as csv_file:
-		data = [row for row in csv.reader(csv_file, delimiter=',')]
-	return data[row]
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		data = list(csv.reader(csv_file, delimiter=','))
+		data = list(filter(None, data))
+		if row > len(data):
+			return ["No data, out of range"]
+		else:
+			return data[row]
 
 def csvWrite(filename, data):
 	with open((filename + ".csv"), mode="w") as csv_file:
-		writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 		writer.writerows(data)
 
 def csvRead(filename):
 	with open((filename + ".csv")) as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
-		line = 0
-		for row in csv_reader:
-			if line == 0:
-				print(f"Columns:{', '.join(row)}")
-				line += 1
-			else:
-				print(f"\t{', '.join(row)}")
+		data = list(csv.reader(csv_file, delimiter=','))
+		data = list(filter(None, data))
+		return data
 
 testData1 = [
 	["Title1","Title2","Title3"],
@@ -46,8 +46,19 @@ testData2 = [
 	[767,"Word6"],
 ]
 
+print("Test writing")
 csvWrite("Test1",testData1)
 csvWrite("Test2",testData2)
 
+print("Test writing one row to a csv")
 csvWriteRow("Test3",testData1[3])
 csvWriteRow("Test4", testData2[6])
+
+print("Test reading table")
+print(csvRead("Test1"))
+print("Test reading one line table")
+print(csvRead("Test4"))
+print("Test reading one row")
+print(csvReadRow("Test1",3))
+print("Test out of limits row")
+print(csvReadRow("Test2",10))
