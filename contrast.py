01fc38d50b5b2c6306db30e220d94d76805d766d
top9 <top9@aber.ac.uk>
2019-04-28 12:02:48 +0100

    Update contrast.py
    Turned it into a module

--- contrast.py
+++ contrast.py
@@ -1,42 +1,40 @@
 import cv2
-import numpy as np
-import imutils
 import Submodules as sb
 import scipy.stats as stats
 
-mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
-filename = ""
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f))]
+def getContrastCSV(img, filename):
+	csvName = "contrastTest"
+	if sb.csvReadRow(csvName,0) == None:
+		datum = ("filename", "Contrast","Brightness","Accepted")
+		sb.csvWriteRow(csvName,datum)
+	space_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-for f in onlyfiles:
-	filename = f
-	img = cv2.imread(filename)
-	height, width, colour = img.shape
+	hist_img = cv2.calcHist([space_img[0]],[0],None,[256],[0,256])
+	contrast = stats.variation(hist_img,None)
+
+	grey = sb.grey(img)
+	ret, thresh = sb.thresh(grey)
+	wcount = sb.whiteCount(thresh)
+
+	accepted = True
+
+	if (contrast-(10*wcount) < 0.1) or (wcount < 0.1):
+		accepted = False
 	
-	dim = sb.setScaling(img)
+	datum = (filename, contrast, (wcount*100), accepted)
+	sb.csvWriteRow(csvName,datum)
+	return accepted
 
-	#grey = sb.grey(img)
+def getContrast(img):
+	space_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 
-	#hist_img = cv2.calcHist([grey],[0],None,[256],[0,256])
-	#contrast = stats.entropy(hist_img)
+	hist_img = cv2.calcHist([space_img[0]],[0],None,[256],[0,256])
+	contrast = stats.variation(hist_img,None)
 
-	imgLAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-	bckg = cv2.blur(imgLAB,(5,5))
-	contrast = 0
-
-	for y in range(height):
-		for x in range(width):
-			if bckg[y,x].any() != 0:
-				contrast += (img[y,x,0]-bckg[y,x])/bckg[y,x]
-
-	contrast /= (height*width)
-
-
-
-	cv2.putText(img, str(contrast), (10, 60),
-		cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-	imgSml = cv2.resize(img, dim)
-	cv2.imshow(filename, imgSml)
-	cv2.waitKey(0)
+	grey = sb.grey(img)
+	ret, thresh = sb.thresh(grey)
+	wcount = sb.whiteCount(thresh)
+	accepted = True
+	if (contrast-(10*wcount) < 0.1) or (wcount < 0.1):
+		accepted = False
+	return accepted
