c5b32ba6a5250609b0458df7951b86cf9ea3c18f
top9 <top9@aber.ac.uk>
2019-04-29 00:14:44 +0100

    Update main.py
    Started implementing modules

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ filename = ""
 blurCheck = horizonCheck = symmetryCheck = False
 colourBalCheck = colourfulCheck = brightCheck = False
 vpCheck = False
-colourBalText = ""
+onlyfiles = []
 colours = [
 	"Red",
 	"Orange",
@@ -27,18 +27,19 @@ colours = [
 ]
 
 def readJPGs(fileLoc):
-	onlyfiles = [f for f in listdir(fileLoc) if (isfile(join(fileLoc, f)) and (".jpg" in f))]
+	onlyfiles = [f for f in listdir(fileLoc) if (isfile(join(fileLoc, f))\
+		 and (".jpg" in f))]
 	return onlyfiles
 
-def runModules():
+def runModules(img):
 	if blurCheck:
-		x=1 #Call blur module
+		isBlurry = m.blur(img)
 	if brightCheck:
-		x=1 #Call brightness module
+		brightnessPercent = m.brightness(img)
 	if colourfulCheck:
-		x=1 #Call colourfulness module
+		isColourful = m.colourScale(img)
 	if colourBalCheck:
-		x=1 #Call colourBalance module with the desired colour
+		colour = showVal.get()
 	if symmetryCheck:
 		x=1 #Call symmetry module
 	if horizonCheck:
@@ -47,35 +48,49 @@ def runModules():
 		x=1 #Call vanishing point module
 	
 
-def onClick():
+def openDir():
 	x=1
 
 def makeWidgets(this):
-	cb_blur = tk.Checkbutton(this, text="Blur detection", variable=blurCheck, onvalue=True, offvalue=False)
+	cb_blur = tk.Checkbutton(this, text="Blur detection", variable=blurCheck,\
+		 onvalue=True, offvalue=False)
 	cb_blur.grid(sticky="W",row=1,column=0)
-	cb_bright = tk.Checkbutton(this, text="Brightness calculation", variable=brightCheck, onvalue=True, offvalue=False)
+	cb_bright = tk.Checkbutton(this, text="Brightness calculation", variable=brightCheck,\
+		 onvalue=True, offvalue=False)
 	cb_bright.grid(sticky="W",row=2,column=0)
-	cb_colourful = tk.Checkbutton(this, text="Colourfulness", variable=colourfulCheck, onvalue=True, offvalue=False)
+	cb_colourful = tk.Checkbutton(this, text="Colourfulness", variable=colourfulCheck,\
+		 onvalue=True, offvalue=False)
 	cb_colourful.grid(sticky="W",row=3,column=0)
-	cb_colourBal = tk.Checkbutton(this, text="Colour Balance", variable=colourBalCheck, onvalue=True, offvalue=False)
+	cb_colourBal = tk.Checkbutton(this, text="Colour Balance", variable=colourBalCheck,\
+		 onvalue=True, offvalue=False)
 	cb_colourBal.grid(sticky="W",row=4,column=0)
-	showVal = tk.StringVar()
-	showVal.set(colours[0])
 	en_colourBal = tk.OptionMenu(this,showVal, *colours)
 	en_colourBal.grid(sticky="W",row=4,column=1)
-	cb_symmetry = tk.Checkbutton(this, text="Symmetry Detection", variable=symmetryCheck, onvalue=True, offvalue=False)
+	cb_symmetry = tk.Checkbutton(this, text="Symmetry Detection", variable=symmetryCheck,\
+		 onvalue=True, offvalue=False)
 	cb_symmetry.grid(sticky="W",row=5,column=0)
-	#cb_horizons = tk.Checkbutton(this, text="Horizon Detection", variable=horizonCheck, onvalue=True, offvalue=False)
+	#cb_horizons = tk.Checkbutton(this, text="Horizon Detection", variable=horizonCheck,\
+	#  onvalue=True, offvalue=False)
 	#cb_horizons.grid(sticky="W",row=6,column=0)
-	#cb_vanishing = tk.Checkbutton(this, text="Vanishing Point Detection", variable=vpCheck, onvalue=True, offvalue=False)
+	#cb_vanishing = tk.Checkbutton(this, text="Vanishing Point Detection", variable=vpCheck,\
+	#  onvalue=True, offvalue=False)
 	#cb_vanishing.grid(sticky="W",row=7,column=0)
+	lbl_Dir = tk.Label(this, text="No album selected")
+	lbl_Dir.grid(sticky="W",row=8,column=0)
+	btn_Dir = tk.Button(this, text="Select album", command=openDir())
+	btn_Dir.grid(sticky="W",row=8,column=1)
+
 
 
 top = tk.Tk()
+showVal = tk.StringVar()
+showVal.set(colours[0])
 top.title("Autophotographer")
 makeWidgets(top)
 #top.directory = tk.filedialog.askdirectory()
 #print (top.directory)
+
+#runModules()
 top.mainloop()
 
 
