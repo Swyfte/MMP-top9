71c1079351e31ef06494f3d94aae9bddf25e3bae
top9 <top9@aber.ac.uk>
2019-04-28 11:32:48 +0100

    Work done on main.py

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import Submodules as sb
 import cv2
-
-mypath = "D:\Arianwen\Documents\GitHub\MMP-top9"
-filename = ""
+import tkinter as tk
+from tkinter import filedialog
 from os import listdir
 from os.path import isfile, join
-onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and (".jpg" in f))]
 
-for f in onlyfiles:
+filename = ""
+blurCheck = colourBalCheck = colourfulCheck = brgtCheck = False
+
+def readJPGs(fileLoc):
+	onlyfiles = [f for f in listdir(fileLoc) if (isfile(join(fileLoc, f)) and (".jpg" in f))]
+	return onlyfiles
+
+def runModules():
+	if blurCheck:
+		x=1
+
+def onClick():
+	x=1
+
+def makecbs(this):
+	cb_blur = tk.Checkbutton(this, text="Blur detection", variable=blurCheck, onvalue=True, offvalue=False)
+	cb_blur.grid(sticky="W",row=1,column=0)
+	cb_brgt = tk.Checkbutton(this, text="Brightness calculation", variable=brgtCheck, onvalue=True, offvalue=False)
+	cb_brgt.grid(sticky="W",row=2,column=0)
+	cb_colourful = tk.Checkbutton(this, text="Colourfulness", variable=colourfulCheck, onvalue=True, offvalue=False)
+	cb_colourful.grid(sticky="W",row=3,column=0)
+	cb_colourBal = tk.Checkbutton(this, text="Colour Balance", variable=colourBalCheck, onvalue=True, offvalue=False)
+	cb_colourBal.grid(sticky="W",row=4,column=0)
+
+
+top = tk.Tk()
+top.title("Autophotographer")
+makecbs(top)
+#top.directory = tk.filedialog.askdirectory()
+#print (top.directory)
+top.mainloop()
+
+
+"""for f in onlyfiles:
 	filename = f
-	img = cv2.imread(filename)
+	img = cv2.imread(filename)"""
