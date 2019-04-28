57025e15f5466315a1a9ad98ab5eb00ae80b01da
top9 <top9@aber.ac.uk>
2019-04-28 12:47:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import Submodules as sb
 import cv2
 import tkinter as tk
 from tkinter import filedialog
@@ -7,7 +6,22 @@ from os.path import isfile, join
 from imutils import build_montages
 
 filename = ""
-blurCheck = colourBalCheck = colourfulCheck = brgtCheck = False
+blurCheck = symmetryCheck = colourBalCheck = colourfulCheck = brightCheck = False
+colourBalText = ""
+colours = [
+	"Red",
+	"Orange",
+	"Yellow",
+	"Green",
+	"Teal",
+	"Cyan",
+	"Blue",
+	"Purple",
+	"Magenta",
+	"Pink",
+	"Brown",
+	"Grey"
+]
 
 def readJPGs(fileLoc):
 	onlyfiles = [f for f in listdir(fileLoc) if (isfile(join(fileLoc, f)) and (".jpg" in f))]
@@ -23,12 +37,16 @@ def onClick():
 def makecbs(this):
 	cb_blur = tk.Checkbutton(this, text="Blur detection", variable=blurCheck, onvalue=True, offvalue=False)
 	cb_blur.grid(sticky="W",row=1,column=0)
-	cb_brgt = tk.Checkbutton(this, text="Brightness calculation", variable=brgtCheck, onvalue=True, offvalue=False)
-	cb_brgt.grid(sticky="W",row=2,column=0)
+	cb_bright = tk.Checkbutton(this, text="Brightness calculation", variable=brightCheck, onvalue=True, offvalue=False)
+	cb_bright.grid(sticky="W",row=2,column=0)
 	cb_colourful = tk.Checkbutton(this, text="Colourfulness", variable=colourfulCheck, onvalue=True, offvalue=False)
 	cb_colourful.grid(sticky="W",row=3,column=0)
 	cb_colourBal = tk.Checkbutton(this, text="Colour Balance", variable=colourBalCheck, onvalue=True, offvalue=False)
 	cb_colourBal.grid(sticky="W",row=4,column=0)
+	en_colourBal = tk.OptionMenu(this,"Red", *colours)
+	en_colourBal.grid(sticky="W",row=4,column=1)
+	cb_symmetry = tk.Checkbutton(this, text="Symmetry Detection", variable=symmetryCheck, onvalue=True, offvalue=False)
+	cb_symmetry.grid(sticky="W",row=5,column=0)
 
 
 top = tk.Tk()
