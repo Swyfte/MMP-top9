23f69adb9ec2025d26f825770bae9f0228918e80
top9 <top9@aber.ac.uk>
2019-04-29 11:02:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from tkinter import filedialog
 from os import listdir, getcwd, path
 from imutils import build_montages
 import Modules as m
+import Submodules as sb
 
 filename = ""
 blurCheck = horizonCheck = symmetryCheck = False
 colourBalCheck = colourfulCheck = brightCheck = False
-vpCheck = csvOut = False
+vpCheck = csvOut = contrastCheck = False
 onlyfiles = []
 colours = [
 	"Red",
@@ -49,6 +50,8 @@ def runModules(img):
 		isBlurry = m.blur(img)
 	if brightCheck:
 		brightnessPercent = m.brightness(img)
+	if contrastCheck:
+		contrast = m.getContrast(img)
 	if colourfulCheck:
 		isColourful = m.colourScale(img)
 	if colourBalCheck:
@@ -84,10 +87,22 @@ def onClick():
 		runFilter()
 
 def runFilter():
-	x=1
+	for f in onlyfiles:
+		filename = f
+		img = cv2.imread(filename)
 
 def runFilterWithCSV():
-	x=1
+	response = tk.simpledialog.askstring("Input", "Enter a name for the output file:",
+                                parent=top)
+	if response is not None:
+		csvName = response
+	else:
+		csvName = "output"
+	datum = ["Filename","Focus","Brightness","Contrast","","","","","",""]
+	sb.csvWriteRow(csvName, datum)
+	for f in onlyfiles:
+		filename = f
+		img = cv2.imread(filename)
 
 top = tk.Tk()
 showVal = tk.StringVar()
@@ -98,41 +113,38 @@ cb_blur=tk.Checkbutton(top,text="Blur detection",variable=blurCheck,\
 cb_blur.grid(sticky="W",row=0,column=0)
 cb_bright=tk.Checkbutton(top,text="Brightness calculation",variable=brightCheck,\
 	onvalue=True,offvalue=False)
-cb_bright.grid(sticky="W",row=1,column=0)
+cb_bright.grid(sticky="W",row=2,column=0)
+cb_contrast=tk.Checkbutton(top,text="Contrast calculation",variable=contrastCheck,\
+	onvalue=True,offvalue=False)
+cb_contrast.grid(sticky="W",row=4,column=0)
 cb_colourful=tk.Checkbutton(top,text="Colour Variance",variable=colourfulCheck,\
 	onvalue=True,offvalue=False)
-cb_colourful.grid(sticky="W",row=2,column=0)
+cb_colourful.grid(sticky="W",row=3,column=0)
 cb_colourBal=tk.Checkbutton(top,text="Major Colour:",variable=colourBalCheck,\
 	onvalue=True,offvalue=False)
-cb_colourBal.grid(sticky="W",row=3,column=0)
+cb_colourBal.grid(sticky="W",row=4,column=0)
 en_colourBal=tk.OptionMenu(top,showVal,*colours)
-en_colourBal.grid(sticky="W",row=3,column=1)
+en_colourBal.grid(sticky="W",row=4,column=1)
 cb_symmetry=tk.Checkbutton(top,text="Symmetry Detection",variable=symmetryCheck,\
 	onvalue=True,offvalue=False)
-cb_symmetry.grid(sticky="W",row=4,column=0)
+cb_symmetry.grid(sticky="W",row=5,column=0)
 #cb_horizons=tk.Checkbutton(top,text="Horizon Detection",variable=horizonCheck,\
 #	onvalue=True,offvalue=False)
-#cb_horizons.grid(sticky="W",row=5,column=0)
+#cb_horizons.grid(sticky="W",row=65,column=0)
 #cb_vanishing=tk.Checkbutton(top,text="Vanishing Point Detection",variable=vpCheck,\
 #	onvalue=True,offvalue=False)
-#cb_vanishing.grid(sticky="W",row=6,column=0)
+#cb_vanishing.grid(sticky="W",row=7,column=0)
 lbl_Dir=tk.Label(top,text="No album selected")
-lbl_Dir.grid(sticky="W",row=7,column=0)
+lbl_Dir.grid(sticky="W",row=8,column=0)
 btn_Dir=tk.Button(top,text="Select album",command=openDir)
-btn_Dir.grid(sticky="W",row=7,column=1)
+btn_Dir.grid(sticky="W",row=8,column=1)
 lblPadding=tk.Label(top)
 lblPadding.grid(row=0,column=2)
 btn_Run=tk.Button(top,text="Run Filter",command=onClick)
-btn_Run.grid(sticky="W",row=7,column=3)
+btn_Run.grid(sticky="W",row=8,column=3)
 cb_output=tk.Checkbutton(top,text="Create output file",variable=csvOut,\
 	onvalue=True,offvalue=False)
-cb_output.grid(sticky="W",row=4,column=1,columnspan=3)
-#print (top.directory)
+cb_output.grid(sticky="W",row=5,column=1,columnspan=3)
 
 #runModules()
 top.mainloop()
-
-
-"""for f in onlyfiles:
-	filename = f
-	img = cv2.imread(filename)"""
